83deb18c7f363244f2cd539eb1b9cc2f2e79b2a9
Pavel Dolezal <pavel.dolezal@nic.cz>
2020-03-30 14:21:36 +0200

    Make dp-runner script compatible with Python 3.5.x

--- src/application/dp-runner.py
+++ src/application/dp-runner.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     program = pathlib.Path(sys.argv[0]).parent.absolute() / "dns-probe-@BACKEND@"
 
     while True:
-        completed_process = subprocess.run([program, *sys.argv[1:]])
+        completed_process = subprocess.run([str(program), *sys.argv[1:]])
         if completed_process.returncode != 1:
             sys.exit(completed_process.returncode)
