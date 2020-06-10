79858cfffdd8202562fc0ed0631a8d130ca57701
Ladislav Lhotka <lhotka@nic.cz>
2020-06-10 15:56:05 +0200

    Add ADAM logo and icon

--- doc/conf.py
+++ doc/conf.py
@@ -156,12 +156,12 @@ html_short_title = 'DNS Probe '+version+' Docs'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "dnsprobe_theme/static/fred-logo.png"
+html_logo = "dnsprobe_theme/static/adam-logo.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "dnsprobe_theme/static/favicon.ico"
+html_favicon = "dnsprobe_theme/static/adam-icon.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
